48968098dd7cb3a16436b2864e10997c7b25db9b
Oleksandr Konstantinov <iamkonst17@gmail.com>
2020-11-25 18:22:41 -0800

    added Path class

---
+++ CPath.cpp
@@ -0,0 +1,8 @@
+//
+//  CPath.cpp
+//  Graphs
+//
+//  Created by Oleksandr Konstantinov on 11/25/20.
+//
+
+#include <stdio.h>
---
+++ CPath.h
@@ -0,0 +1,12 @@
+//
+//  CPath.h
+//  Graphs
+//
+//  Created by Oleksandr Konstantinov on 11/25/20.
+//
+
+#ifndef CPath_h
+#define CPath_h
+
+
+#endif /* CPath_h */
